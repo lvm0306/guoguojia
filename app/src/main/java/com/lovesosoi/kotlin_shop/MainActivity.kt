b78a039183e388e3965107ee86795b7e89973054
lovesosoi <842463468@qq.com>
2019-07-10 16:57:26 +0800

    Initial commit

---
+++ app/src/main/java/com/lovesosoi/kotlin_shop/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.lovesosoi.kotlin_shop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import butterknife.ButterKnife
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
